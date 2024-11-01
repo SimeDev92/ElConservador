75780042bb60a0698b25b5466eda2bca529ee0a5
SimeDev <simedev92@gmail.com>
2024-11-01 19:08:17 +0100

    editado archivo config prod

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,10 +1,18 @@
 export const environments = {
-  baseUrl: 'https://my-api-in-prod.com/api',
+  stripePublicKey: 'pk_live_51Q8PMCDZG49tH5nmKTgoC5WA0ByV94c4MCmB5kBHfq36QKBHxClS2g2XAoxCnmIKh3fOZU5RaQ98L9bV5oh3vUaR008qYcD8Jg',
+  stripePublicKeyTest: 'pk_test_51Q8PMCDZG49tH5nm6NLFAdhmzE3q3S4WCOPVdvsPx1wDQoDAwXeyZ3bupDB4yfXMnT1XpjqUw2K9Z4TyHNLrAaNy00JWfc9VlM',
+
+  baseUrl: 'https://el-conservador-cogocyczi-simedevs-projects.vercel.app',
   production: true,
-  useTestProducts: false,
-  facebookUrl: 'https://www.facebook.com',
-  twitterUrl: 'https://www.twitter.com',
-  telegramUrl: 'https://t.me/+V83a31FFvTYxYjY8'
+  useTestProducts: true,
+  facebookUrl: 'https://www.facebook.com/profile.php?id=61567155161620',
+  twitterUrl: 'https://x.com/ConservadorTest',
+  telegramUrl: 'https://t.me/+V83a31FFvTYxYjY8',
+
+
+  contactEmail: 'info@elconservador.com',
+  centinelaBotUrl: 'https://t.me/Centinela_QA_bot'
 }
+
 
 
