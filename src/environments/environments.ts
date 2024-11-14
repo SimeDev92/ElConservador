c0a03da2f993f5830cd002d9d7e4875ed3d0ad8f
SimeDev <simedev92@gmail.com>
2024-11-14 23:24:28 +0100

    Merge QA into main: Update assets, environments, and index

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -2,7 +2,7 @@ export const environments = {
   stripePublicKey: 'pk_live_51Q8PMCDZG49tH5nmKTgoC5WA0ByV94c4MCmB5kBHfq36QKBHxClS2g2XAoxCnmIKh3fOZU5RaQ98L9bV5oh3vUaR008qYcD8Jg',
   stripePublicKeyTest: 'pk_test_51Q8PMCDZG49tH5nm6NLFAdhmzE3q3S4WCOPVdvsPx1wDQoDAwXeyZ3bupDB4yfXMnT1XpjqUw2K9Z4TyHNLrAaNy00JWfc9VlM',
 
-  baseUrl: 'https://el-conservador-backend-5740091828.us-central1.run.app', // 'https://elconservadorback.onrender.com',
+  baseUrl: 'https://el-conservador-backend-5740091828.us-central1.run.app',
   production: false,
   useTestProducts: true,
   facebookUrl: 'https://www.facebook.com/profile.php?id=61567155161620',
