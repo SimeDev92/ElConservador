5c4c677f612f11866679e781473ce1385caf0496
SimeDev <simedev92@gmail.com>
2025-03-03 17:55:46 +0100

    Setting env backend url

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,6 @@
 export const environments = {
   frontendUrl: 'https://elconservadornoticias.com',
-  baseUrl: 'http://localhost:3000',
+  baseUrl: 'https://el-conservador-noticias-backend.vercel.app',
   facebookUrl: 'https://www.facebook.com/people/El-Conservador-Noticias/61571265990971/',
   twitterUrl: 'https://x.com/Conservador24h',
   telegramUrl: 'https://t.me/+V83a31FFvTYxYjY8',
